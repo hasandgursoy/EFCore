30a518fd8f839d182d3b47a0792208b19d03702b
hasandgursoy <hasan.d.gursoy@gmail.com>
2022-06-28 16:46:49 +0300

    Eager Loading and Explicit Loading

--- EFCore.CodeFirstOneToMany/Program.cs
+++ EFCore.CodeFirstOneToMany/Program.cs
@@ -10,11 +10,30 @@ Initializer.Build();
 
 using (var _context = new AppDbContext())
 {
+    // Eager Loading
 
     // Eager Loading = Veriyi alırken beraberinde bağımlı başka verileri de istemek.
-    var categoryWithProducts = _context.Categories.Include(x => x.Products).ThenInclude(x => x.ProductFeature);
+    //var categoryWithProducts = _context.Categories.Include(x => x.Products).ThenInclude(x => x.ProductFeature);
 
 
+    // Explicit Loading
+
+    // Explicit Loading = İhtiyaç durumuna göre sonra çekme
+    //var category = _context.Categories.First();
+
+    //if (true)
+    //{
+    //    Çoka Çok ilişkilerde Collection İfadesi
+    //    _context.Entry(category).Collection(x => x.Products).Load();
+
+    //}
+
+    //var product = _context.Products.First();
+
+    //if (true)
+    //{   // Birebir bir ilişkiler de Reference
+    //    _context.Entry(product).Reference(x => x.ProductFeature).Load();
+    //}
 
 
 }
